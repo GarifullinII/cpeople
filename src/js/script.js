6f1034548607b942818da48621e1b6509f5182b5
Ildar Garifullin <ildaridrisovich@gmail.com>
2021-01-21 15:10:01 +0400

    внес изменения в php и js файлы

--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,29 @@
+const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    closeElem = document.querySelector('.menu__close');
 
-    const hamburger = document.querySelector('.hamburger'),
-        menu = document.querySelector('.menu'),
-        closeElem = document.querySelector('.menu__close');
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
 
-    hamburger.addEventListener('click', () => {
-        menu.classList.add('active');
-    });
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
 
-    closeElem.addEventListener('click', () => {
-        menu.classList.remove('active');
-    });
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+    lines = document.querySelectorAll('.skills__ratings-line span');
 
-    const counters = document.querySelectorAll('.skills__ratings-counter'),
-        lines = document.querySelectorAll('.skills__ratings-line span');
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
 
-    counters.forEach( (item, i) => {
-        lines[i].style.width = item.innerHTML;
-    });
-
-    $('form').submit(function(e) {
-        e.preventDefault();
-        $.ajax({
-            type: "POST",
-            url: "smart.php",
-            data: $(this).serialize()
-        }).done(function() {
-            $(this).find("input").val("");
-
-            $('form').trigger('reset');
-            return false;
-        });
-    });
+const XHR = new XMLHTTPRequest ()
+    function sendMail (f) {
+        const str = [] 
+            for (var el = f.elements, j = 0; j < el.length - 1; j++)
+                {
+                str [j] = el[j]. name + '=' + encodeURIComponent (el[j].value);
+                }
+            XHR.open ('get', 'smart.php?' + str.join ('&'), true);
+            XHR.send (null);
+        }
