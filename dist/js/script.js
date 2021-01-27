e59e4e405a2da385904dfbf55973e35b34806c0e
Ildar Garifullin <ildaridrisovich@gmail.com>
2021-01-27 19:16:36 +0400

    внес изменения в js файл

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,29 +1,33 @@
-const hamburger = document.querySelector('.hamburger'),
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
     closeElem = document.querySelector('.menu__close');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
-});
+    });
 
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
-});
+    });
 
 const counters = document.querySelectorAll('.skills__ratings-counter'),
     lines = document.querySelectorAll('.skills__ratings-line span');
 
 counters.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
-});
+    });
 
 const XHR = new XMLHTTPRequest ()
-function sendMail (f) {
-const str = [] 
+    function sendMail (f) {
+    const str = [] 
     for (var el = f.elements, j = 0; j < el.length - 1; j++)
         {
         str [j] = el[j]. name + '=' + encodeURIComponent (el[j].value);
         }
     XHR.open ('get', 'smart.php?' + str.join ('&'), true);
     XHR.send (null);
-    }
+    };
+});
